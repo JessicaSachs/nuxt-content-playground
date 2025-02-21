a2969abae963905b69087b49c8610b1ac534b901
Jessica Sachs <jess@jessicasachs.io>
2025-02-21 11:44:09 -0600

    loading remote README.md from Vue.js repository

--- content.config.ts
+++ content.config.ts
@@ -1,6 +1,13 @@
 import { defineCollection } from '@nuxt/content'
 
 export const collections = {
+  vuejs: defineCollection({
+    source: {
+      include: 'README.md',
+      repository: 'https://github.com/vuejs/core',
+    },
+    type: 'page'
+  }),
   content: defineCollection({
     source: '**/*.md',
     type: 'page'
